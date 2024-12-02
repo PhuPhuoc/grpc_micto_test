a20a7b027b48517b4ea0bed788a956f437b9ef54
unknown <blessforwork@gmail.com>
2024-12-02 16:30:00 +0700

    edit swagger, add cors config, finish test grpc 1

--- gateway/main.go
+++ gateway/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/PhuPhuoc/grpc_micro_test-gateway/internal/handler"
 	authenpb "github.com/PhuPhuoc/grpc_micro_test-gateway/proto/authen"
 	userpb "github.com/PhuPhuoc/grpc_micro_test-gateway/proto/user"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -18,7 +19,6 @@ import (
 //	@version	1.0
 
 func main() {
-	docs.SwaggerInfo.BasePath = "/api/v1"
 	// Kết nối tới Authen Service
 	authenConn, err := grpc.Dial("authen-service:50051", grpc.WithInsecure())
 	if err != nil {
@@ -37,18 +37,29 @@ func main() {
 	authenClient := authenpb.NewAuthServiceClient(authenConn)
 	userClient := userpb.NewUserServiceClient(userConn)
 
-	// Khởi tạo router Gin
-	router := gin.Default()
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	// Sử dụng middleware CORS
-	router.Use(
-		gin.LoggerWithWriter(gin.DefaultWriter, "/swagger/*any"),
-		gin.Recovery(),
-	)
 	apiHandler := &handler.APIHandler{
 		AuthenClient: authenClient,
 		UserClient:   userClient,
 	}
+
+	// Khởi tạo router Gin
+	router := gin.New()
+	// CORS middleware (allow all origins)
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	router.Use(
+		gin.LoggerWithWriter(gin.DefaultWriter, "/swagger/*any"),
+		gin.Recovery(),
+	)
+
+	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	// Định nghĩa các endpoint
 	router.POST("/api/v1/register", apiHandler.Register)
@@ -56,6 +67,6 @@ func main() {
 
 	// Chạy Gateway
 	log.Println("API Gateway running on port 8080...")
-	fmt.Printf("\nFor development: http://localhost%s/swagger/index.html\n\n", "8080")
+	fmt.Printf("\nFor development: http://localhost%s/swagger/index.html\n\n", ":8080")
 	router.Run(":8080")
 }
